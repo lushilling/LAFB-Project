9bfa7c0774099d2ecba0fef4a3830efdc953bb67
Ubuntu <adminuser@LocalVM.xd05lcrczc1eljhkrltvjn3ozh.cwx.internal.cloudapp.net>
2019-07-18 10:53:46 +0000

    made micro-services

---
+++ text_gen/app.py
@@ -0,0 +1,32 @@
+#!flask/bin/python
+from flask import Flask, jsonify, make_response
+import sys
+import requests
+import string
+import random
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "test"
+
+@app.route('/textgen', methods=['GET'])
+def randomString(stringLength=3):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+@app.route('/anEndpoint')
+def make_request():
+    return requests.get('http://example.com').content
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=9002)
+
+
+
+
---
+++ text_genUpper/README.md
@@ -0,0 +1,5 @@
+TO RUN :
+
+pip install flask
+
+python app.py
